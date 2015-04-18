f6538d04ef076fa66981b7361bcb76bee02a58de
Mustafa Mustafa <mstftsm@gmail.com>
2015-04-18 03:12:32 -0400

    remove StHFCuts related details for this branch only

--- StRoot/macros/runPicoD0AnaMaker.C
+++ StRoot/macros/runPicoD0AnaMaker.C
@@ -16,7 +16,6 @@ void runPicoD0AnaMaker(TString d0list, TString outFileName, TString badRunListFi
    gSystem->Load("StPicoPrescales");
    gSystem->Load("StPicoD0EventMaker");
    gSystem->Load("StPicoD0AnaMaker");
-   gSystem->Load("StPicoHFMaker");
 
    chain = new StChain();
 
@@ -28,33 +27,7 @@ void runPicoD0AnaMaker(TString d0list, TString outFileName, TString badRunListFi
    StPicoDstMaker* picoDstMaker = new StPicoDstMaker(0, "correspondingPico.list", "picoDstMaker");
    StPicoD0AnaMaker*  picoD0AnaMaker = new StPicoD0AnaMaker("picoD0AnaMaker", d0list, outFileName.Data(), picoDstMaker);
 
-   StHFCuts* d0Cuts = new StHFCuts("d0Cuts");
-   picoD0AnaMaker->setHFCuts(d0Cuts);
-
    // -------------- USER variables -------------------------
-
-   // -- File name of bad run list
-   d0Cuts->setBadRunListFileName(badRunListFileName);
-
-   // add your cuts here.
-
-   // tracking
-   d0Cuts->setCutNHitsFitMax(20);
-
-   // pions
-   d0Cuts->setCutTPCNSigmaPion(3.0);
-
-   // kaons
-   d0Cuts->setCutTPCNSigmaKaon(2.0);
-
-   // kaonPion pair cuts
-   float dcaDaughtersMax = 0.008;  // maximum
-   float decayLengthMin  = 0.0030; // minimum
-   float decayLengthMax  = 999999; //std::numeric_limits<float>::max();
-   float cosThetaMin     = 0.90;   // minimum
-   float minMass         = 1.6;
-   float maxMass         = 2.1;
-   d0Cuts->setCutSecondaryPair(dcaDaughtersMax, decayLengthMin, decayLengthMax, cosThetaMin, minMass, maxMass);
 
    chain->Init();
    int nEntries = picoD0AnaMaker->getEntries();
