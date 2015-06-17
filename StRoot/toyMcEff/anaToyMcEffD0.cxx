17a836e8fffde602b6d518424f6564008225da01
Mustafa Mustafa <mstftsm@gmail.com>
2015-06-17 11:42:58 -0700

    apply astyle

--- StRoot/toyMcEff/anaToyMcEffD0.cxx
+++ StRoot/toyMcEff/anaToyMcEffD0.cxx
@@ -35,90 +35,90 @@ using namespace std;
 
 int getD0PtIndex(float const pt)
 {
-  int bin = -1;
-  for(int i=0; i < anaCuts::nPtBins; i++) 
-  {
-    if( (pt >= anaCuts::PtEdge[i]) && (pt < anaCuts::PtEdge[i+1]) )
-     bin = i;
-  }
-  return bin;
+   int bin = -1;
+   for (int i = 0; i < anaCuts::nPtBins; i++)
+   {
+      if ((pt >= anaCuts::PtEdge[i]) && (pt < anaCuts::PtEdge[i + 1]))
+         bin = i;
+   }
+   return bin;
 }
 
 bool isGoodPair(float const pt, float const cosTheta, float const pDca, float const kDca,
                 float const dca12, float const decayLength, float const dcaV0ToPv)
 {
-  int tmpIndex = getD0PtIndex(pt);
+   int tmpIndex = getD0PtIndex(pt);
 
-  return cosTheta > anaCuts::cosTheta[tmpIndex] &&
-    pDca > anaCuts::pDca[tmpIndex] && kDca > anaCuts::kDca[tmpIndex] &&
-    dca12 < anaCuts::dcaDaughters[tmpIndex] &&
-    decayLength > anaCuts::decayLength[tmpIndex] &&
-    dcaV0ToPv < anaCuts::dcaV0ToPv[tmpIndex];
+   return cosTheta > anaCuts::cosTheta[tmpIndex] &&
+          pDca > anaCuts::pDca[tmpIndex] && kDca > anaCuts::kDca[tmpIndex] &&
+          dca12 < anaCuts::dcaDaughters[tmpIndex] &&
+          decayLength > anaCuts::decayLength[tmpIndex] &&
+          dcaV0ToPv < anaCuts::dcaV0ToPv[tmpIndex];
 }
 
 int main(int argc, char **argv)
 {
    d0Nt* t = new d0Nt();
 
-   TFile* fOut = new TFile("eff.root","recreate");
+   TFile* fOut = new TFile("eff.root", "recreate");
 
-   TH1D* noCuts010 = new TH1D("noCuts010","",60,0,12);
-   TH1D* noCuts4080 = new TH1D("noCuts4080","",60,0,12);
+   TH1D* noCuts010 = new TH1D("noCuts010", "", 60, 0, 12);
+   TH1D* noCuts4080 = new TH1D("noCuts4080", "", 60, 0, 12);
 
-   TH1D* topoOnly010= new TH1D("topoOnly010","",60,0,12);
-   TH1D* topoOnly4080= new TH1D("topoOnly4080","",60,0,12);
+   TH1D* topoOnly010 = new TH1D("topoOnly010", "", 60, 0, 12);
+   TH1D* topoOnly4080 = new TH1D("topoOnly4080", "", 60, 0, 12);
 
-   TH1D* hftOnly010= new TH1D("hftOnly010","",60,0,12);
-   TH1D* hftOnly4080= new TH1D("hftOnly4080","",60,0,12);
+   TH1D* hftOnly010 = new TH1D("hftOnly010", "", 60, 0, 12);
+   TH1D* hftOnly4080 = new TH1D("hftOnly4080", "", 60, 0, 12);
 
-   TH1D* hftTopo010= new TH1D("hftTopo010","",60,0,12);
-   TH1D* hftTopo4080= new TH1D("hftTopo4080","",60,0,12);
+   TH1D* hftTopo010 = new TH1D("hftTopo010", "", 60, 0, 12);
+   TH1D* hftTopo4080 = new TH1D("hftTopo4080", "", 60, 0, 12);
 
-   TH1D* hftTpc010= new TH1D("hftTpc010","",60,0,12);
-   TH1D* hftTpc4080= new TH1D("hftTpc4080","",60,0,12);
+   TH1D* hftTpc010 = new TH1D("hftTpc010", "", 60, 0, 12);
+   TH1D* hftTpc4080 = new TH1D("hftTpc4080", "", 60, 0, 12);
 
    for (Long64_t i = 0; i < t->GetEntries(); ++i)
    {
       t->GetEntry(i);
 
-      if(!(t->cent>=7 || t->cent<4)) continue;
+      if (!(t->cent >= 7 || t->cent < 4)) continue;
 
-      if(t->cent>=7) noCuts010->Fill(t->rPt);
-      else if (t->cent<4) noCuts4080->Fill(t->rPt);
+      if (t->cent >= 7) noCuts010->Fill(t->rPt);
+      else if (t->cent < 4) noCuts4080->Fill(t->rPt);
 
-      bool passTopologicalCuts = isGoodPair(t->rPt,t->cosTheta,t->pRDca,t->kRDca,t->dca12,t->decayLength,t->dcaD0ToPv);
-      if(!passTopologicalCuts) continue;
+      bool passTopologicalCuts = isGoodPair(t->rPt, t->cosTheta, t->pRDca, t->kRDca, t->dca12, t->decayLength, t->dcaD0ToPv);
+      if (!passTopologicalCuts) continue;
 
-      bool passHft = t->kHft>0 && t->pHft>0;
-      bool passTpc = t->reco>0;
+      bool passHft = t->kHft > 0 && t->pHft > 0;
+      bool passTpc = t->reco > 0;
 
-      if(t->cent>=7) // 0-10
+      if (t->cent >= 7) // 0-10
       {
-        if(passHft) hftOnly010->Fill(t->rPt);
-        if(passTopologicalCuts) topoOnly010->Fill(t->rPt);
-        if(passTopologicalCuts && passHft) hftTopo010->Fill(t->rPt);
-        if(passTopologicalCuts && passHft && passTpc) hftTpc010->Fill(t->rPt);
+         if (passHft) hftOnly010->Fill(t->rPt);
+         if (passTopologicalCuts) topoOnly010->Fill(t->rPt);
+         if (passTopologicalCuts && passHft) hftTopo010->Fill(t->rPt);
+         if (passTopologicalCuts && passHft && passTpc) hftTpc010->Fill(t->rPt);
       }
-      else if(t->cent<4) // 40-80
+      else if (t->cent < 4) // 40-80
       {
-        if(passHft) hftOnly4080->Fill(t->rPt);
-        if(passTopologicalCuts) topoOnly4080->Fill(t->rPt);
-        if(passTopologicalCuts && passHft) hftTopo4080->Fill(t->rPt);
-        if(passTopologicalCuts && passHft && passTpc) hftTpc4080->Fill(t->rPt);
+         if (passHft) hftOnly4080->Fill(t->rPt);
+         if (passTopologicalCuts) topoOnly4080->Fill(t->rPt);
+         if (passTopologicalCuts && passHft) hftTopo4080->Fill(t->rPt);
+         if (passTopologicalCuts && passHft && passTpc) hftTpc4080->Fill(t->rPt);
       }
    } // end event looping
 
-   noCuts010->Scale(1/2.);
-   topoOnly010->Scale(1/2.);
-   hftOnly010->Scale(1/2.);
-   hftTopo010->Scale(1/2.);
-   hftTpc010->Scale(1/2.);
+   noCuts010->Scale(1 / 2.);
+   topoOnly010->Scale(1 / 2.);
+   hftOnly010->Scale(1 / 2.);
+   hftTopo010->Scale(1 / 2.);
+   hftTpc010->Scale(1 / 2.);
 
-   noCuts4080->Scale(1/4.);
-   topoOnly4080->Scale(1./4.);
-   hftOnly4080->Scale(1./4.);
-   hftTopo4080->Scale(1/4.);
-   hftTpc4080->Scale(1./4.);
+   noCuts4080->Scale(1 / 4.);
+   topoOnly4080->Scale(1. / 4.);
+   hftOnly4080->Scale(1. / 4.);
+   hftTopo4080->Scale(1 / 4.);
+   hftTpc4080->Scale(1. / 4.);
 
    noCuts010->Sumw2();
    noCuts4080->Sumw2();
