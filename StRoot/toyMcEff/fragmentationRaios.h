a3d580c0981e16562a7d874fe7324a92c7363785
Mustafa Mustafa <mstftsm@gmail.com>
2015-06-22 09:11:00 -0700

    add a table of fragmentation ratios

---
+++ StRoot/toyMcEff/fragmentationRaios.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <utility>
+
+using namespace std;
+
+void printFragmentationRatios()
+{
+  cout<<"ZEUS Collaboration - arXiv:hep-ex/0508019 - Table 4."<<endl;
+  cout<<"f(c → D+) 0.217 ± 0.014"<<endl;
+  cout<<"f(c → D0) 0.523 ± 0.021"<<endl;
+  cout<<"f(c → D+s) 0.095 ± 0.008"<<endl;
+  cout<<"f(c → Λ+c) 0.144 ± 0.022"<<endl;
+  cout<<"f(c → D∗+) 0.200 ± 0.009"<<endl;
+}
+
+namespace fragmentationRatios
+{
+  std::pair<double,double> const charmToDPlusMinus = make_pair(0.217,0.014);
+  std::pair<double,double> const charmToDZero      = make_pair(0.523,0.021);
+  std::pair<double,double> const charmToDs         = make_pair(0.095, 0.008);
+  std::pair<double,double> const charmToLambdaC    = make_pair(0.144,0.022);
+  std::pair<double,double> const charmToDStar      = make_pair(0.200,0.009);
+}
