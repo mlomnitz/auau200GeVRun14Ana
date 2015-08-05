984859f9ad416a7f0145c0533f090ea36aa2ec29
Mustafa Mustafa <mstftsm@gmail.com>
2015-08-05 14:36:59 -0700

    add dedxMethod

--- StRoot/StMcAnalysisMaker/StMcAnaCuts.h
+++ StRoot/StMcAnalysisMaker/StMcAnaCuts.h
@@ -11,11 +11,14 @@
 #include <vector>
 
 #include "Rtypes.h"
+#include "StEvent/StEnumerations.h"
 
 namespace McAnaCuts
 {
   std::vector<unsigned int> interesting_triggers;
   float const mcTrackStartVtxR = 1.0; // maximum
   int const geantId = 8;
+
+  StDedxMethod dedxMethod = kLikelihoodFitId;
 }
 #endif
