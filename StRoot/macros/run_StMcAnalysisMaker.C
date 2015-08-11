24e370baab3464948ce487ac48e9db87159bc0f8
Mustafa Mustafa <mstftsm@gmail.com>
2015-08-11 14:40:09 -0700

    add gRefMultCorr and run astyle

--- StRoot/macros/run_StMcAnalysisMaker.C
+++ StRoot/macros/run_StMcAnalysisMaker.C
@@ -5,93 +5,112 @@ void Load();
 
 using namespace std;
 
-void run_StMcAnalysisMaker(const char* file, std::string outFile="test")
+void run_StMcAnalysisMaker(const char* file, std::string outFile = "test")
 {
-    //Check STAR Library. Please set SL_version to the original star library used 
-    // in the production from http://www.star.bnl.gov/devcgi/dbProdOptionRetrv.pl
-    
-    string SL_version = "SL15c";
-    string env_SL = getenv("STAR");
+   //Check STAR Library. Please set SL_version to the original star library used
+   // in the production from http://www.star.bnl.gov/devcgi/dbProdOptionRetrv.pl
 
-    if (env_SL.find(SL_version) == string::npos)
-    {
-        cout << "Environment Star Library does not match the requested library in run_st_etree.C. Exiting..." << endl;
-        return;
-    }
+   string SL_version = "SL15c";
+   string env_SL = getenv("STAR");
 
-    // Load shared libraries
-    Load();
+   if (env_SL.find(SL_version) == string::npos)
+   {
+      cout << "Environment Star Library does not match the requested library in run_st_etree.C. Exiting..." << endl;
+      return;
+   }
 
-    // Create chain
-    StChain* chain = new StChain;
+   // Load shared libraries
+   Load();
 
-    // I/O maker
-    StIOMaker* ioMaker = new StIOMaker;
-    ioMaker->SetFile(file);
-    ioMaker->SetIOMode("r");
-    ioMaker->SetBranch("*", 0, "0");
-    //ioMaker->SetBranch("McEventBranch",0,"r"); 
-    ioMaker->SetBranch("geantBranch", 0, "r");
-    ioMaker->SetBranch("eventBranch", 0, "r");
+   // Create chain
+   StChain* chain = new StChain;
 
-    TString mudstfile = file;
-    mudstfile.ReplaceAll(".event.root",".MuDst.root");
-    mudstfile.ReplaceAll(".geant.root",".MuDst.root");
-    StMuDstMaker* muDstMaker = new StMuDstMaker(0,0,"",mudstfile.Data(),"",100000,"MuDst");
+   // I/O maker
+   StIOMaker* ioMaker = new StIOMaker;
+   ioMaker->SetFile(file);
+   ioMaker->SetIOMode("r");
+   ioMaker->SetBranch("*", 0, "0");
+   //ioMaker->SetBranch("McEventBranch",0,"r");
+   ioMaker->SetBranch("geantBranch", 0, "r");
+   ioMaker->SetBranch("eventBranch", 0, "r");
 
-    StMcEventMaker *mcEventMaker = new StMcEventMaker();
-    mcEventMaker->doPrintEventInfo = false;
-    mcEventMaker->doPrintMemoryInfo = false;
+   TString mudstfile = file;
+   mudstfile.ReplaceAll(".event.root", ".MuDst.root");
+   mudstfile.ReplaceAll(".geant.root", ".MuDst.root");
+   StMuDstMaker* muDstMaker = new StMuDstMaker(0, 0, "", mudstfile.Data(), "", 100000, "MuDst");
 
-    StAssociationMaker* assoc = new StAssociationMaker;
-    assoc->useInTracker();
-    assoc->SetDebug();
+   StMcEventMaker *mcEventMaker = new StMcEventMaker();
+   mcEventMaker->doPrintEventInfo = false;
+   mcEventMaker->doPrintMemoryInfo = false;
 
-    //.. see example in CVS: StRoot/macros/mudst/exampleEmc.C
-    // Need St_db_Maker for Emc calibration
-    // St_db_Maker* dbMk = new St_db_Maker("StarDb", "MySQL:StarDb");
-    //dbMk->SetMaxEntryTime(20100301,0);
-    //dbMk->SetDateTime(20080101,000001);
+   StAssociationMaker* assoc = new StAssociationMaker;
+   assoc->useInTracker();
+   assoc->SetDebug();
 
-    // Monte Carlo event maker
-    StMcAnalysisMaker* analysis = new StMcAnalysisMaker;
-    analysis->setOutFileName(outFile);
-    analysis->fillTpcHitsNtuple();
+   //.. see example in CVS: StRoot/macros/mudst/exampleEmc.C
+   // Need St_db_Maker for Emc calibration
+   // St_db_Maker* dbMk = new St_db_Maker("StarDb", "MySQL:StarDb");
+   //dbMk->SetMaxEntryTime(20100301,0);
+   //dbMk->SetDateTime(20080101,000001);
+   int refMultCorrLoad = gSystem->Load("StRefMultCorr");
+   StRefMultCorr* grefmultCorrUtil = NULL;
 
-    // Initialize chain
-    chain->Init();
-    chain->EventLoop(1e6);
-    chain->Finish();
+   if (refMultCorrLoad == -1)
+   {
+      cout << "StRefMultCorr library is not available" << endl;
+   }
+   else
+   {
+      grefmultCorrUtil = CentralityMaker::instance()->getgRefMultCorr();
+      grefmultCorrUtil->setVzForWeight(6, -6.0, 6.0);
+      grefmultCorrUtil->readScaleForWeight("StRoot/StRefMultCorr/macros/weight_grefmult_vpd30_vpd5_Run14.txt");
 
-    //delete chain;
+      for (int i = 0; i < 6; ++i)
+      {
+         cout << i << " " << grefmultCorrUtil->get(i, 0) << endl;
+      }
+   }
+
+   // Monte Carlo event maker
+   StMcAnalysisMaker* analysis = new StMcAnalysisMaker;
+   analysis->setOutFileName(outFile);
+   analysis->fillTpcHitsNtuple();
+   analysis->setRefMultCorr(grefmultCorrUtil);
+
+   // Initialize chain
+   chain->Init();
+   chain->EventLoop(1e6);
+   chain->Finish();
+
+   //delete chain;
 }
 
-void Load() 
+void Load()
 {
-  gROOT->Macro("loadMuDst.C");
-  gROOT->Macro("LoadLogger.C");
-  gSystem->Load("StMcEvent");
-  gSystem->Load("StMcEventMaker");
-  gSystem->Load("StAssociationMaker");
-  gSystem->Load("StDbLib.so");
-  gSystem->Load("StDbBroker.so");
-  gSystem->Load("libglobal_Tables.so");
-  gSystem->Load("St_db_Maker.so");
-  gSystem->Load("StDetectorDbMaker");
-  gSystem->Load("StTpcDb");
-  gSystem->Load("StDbUtilities");
-  gSystem->Load("StMcEvent");
-  gSystem->Load("StMcEventMaker");
-  gSystem->Load("StAssociationMaker");
-  gSystem->Load("StEmcRawMaker");
-  gSystem->Load("StEmcADCtoEMaker");
-  gSystem->Load("StPreEclMaker");
-  gSystem->Load("StEpcMaker");
-  gSystem->Load("StEmcSimulatorMaker");
-  gSystem->Load("StEmcUtil");
-  gSystem->Load("StEEmcUtil");
-  gSystem->Load("StEEmcDbMaker");
-  gSystem->Load("StEmcTriggerMaker");
-  gSystem->Load("StDaqLib");
-  gSystem->Load("StMcAnalysisMaker");
+   gROOT->Macro("loadMuDst.C");
+   gROOT->Macro("LoadLogger.C");
+   gSystem->Load("StMcEvent");
+   gSystem->Load("StMcEventMaker");
+   gSystem->Load("StAssociationMaker");
+   gSystem->Load("StDbLib.so");
+   gSystem->Load("StDbBroker.so");
+   gSystem->Load("libglobal_Tables.so");
+   gSystem->Load("St_db_Maker.so");
+   gSystem->Load("StDetectorDbMaker");
+   gSystem->Load("StTpcDb");
+   gSystem->Load("StDbUtilities");
+   gSystem->Load("StMcEvent");
+   gSystem->Load("StMcEventMaker");
+   gSystem->Load("StAssociationMaker");
+   gSystem->Load("StEmcRawMaker");
+   gSystem->Load("StEmcADCtoEMaker");
+   gSystem->Load("StPreEclMaker");
+   gSystem->Load("StEpcMaker");
+   gSystem->Load("StEmcSimulatorMaker");
+   gSystem->Load("StEmcUtil");
+   gSystem->Load("StEEmcUtil");
+   gSystem->Load("StEEmcDbMaker");
+   gSystem->Load("StEmcTriggerMaker");
+   gSystem->Load("StDaqLib");
+   gSystem->Load("StMcAnalysisMaker");
 }
