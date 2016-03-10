4e456a1b188e24546fbf23c868f166ededd7b5c3
Mustafa Mustafa <mstftsm@gmail.com>
2016-03-10 13:12:39 -0800

    add a utility function to determine PXL sector

--- StRoot/StPicoVtxAnaMaker/StCuts.h
+++ StRoot/StPicoVtxAnaMaker/StCuts.h
@@ -28,5 +28,20 @@ namespace cuts
    // vertex refit track quality
    float const vtxDca = 3.0;
    size_t const vtxNumberOfFitPoints = 20;
+
+   int const nPxlPhiEdges = 11;
+   float const PxlPhiEdges[nPxlPhiEdges + 1] = {-3.14159 , -2.80359 , -2.17527 , -1.54696 , -0.918637 , -0.290319 , 0.338 , 0.966319 , 1.59464 , 2.22296 , 2.85127 , 3.14159};
+   int const PxlSectorNumber[nPxlPhiEdges + 1] = {3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3};
+
+   int getPxlSectorNumber(float const phi)
+   {
+     for (int i = 0; i < nPxlPhiEdges; ++i)
+     {
+       if (phi > PxlPhiEdges[i] && phi < PxlPhiEdges[i+1])
+       {
+         return PxlSectorNumber[i];
+       }
+     }
+   }
 }
 #endif
